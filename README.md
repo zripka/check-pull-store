c7b63fa0dbf333236a0480814dbcf244603bb977
zripka <zripka@gmail.com>
2024-07-07 03:15:07 -0400

    code works, waiting for API to update before testing

---
+++ README.md
@@ -0,0 +1,1 @@
+Boilerplate code for a common gesture: reach some resource, check if it has changed, store the updated information.
--- check_pull_store.py
+++ check_pull_store.py
@@ -1,6 +1,37 @@
 import json
 import subprocess
+import pprint
+import sqlite3
 
 toronto_weather = json.loads(subprocess.check_output('curl "http://api.weatherapi.com/v1/current.json?key=$(cat .config)&q=toronto&aqi=no"', shell=True))
 
-print(toronto_weather)
+try:
+    connection = sqlite3.connect("file:temperature.db?mode=rw", uri=True)
+    cursor = connection.cursor()
+except sqlite3.OperationalError:
+    connection = sqlite3.connect("temperature.db")
+    cursor = connection.cursor()
+    cursor.execute("""
+        create table temperature (
+            record_id integer, 
+            last_updated text, 
+            temperature_c real, 
+            primary key (record_id), 
+            unique (last_updated)
+        );
+    """)
+    connection.commit()
+
+query = """
+    insert or ignore into temperature (last_updated, temperature_c)
+    values
+    ('{}', '{}')
+"""
+cursor.execute(query.format(toronto_weather['current']['last_updated'], toronto_weather['current']['temp_c']))
+
+connection.commit()
+
+subprocess.run('sqlite3 -column -header temperature.db "select * from temperature;"', shell=True)
+
+cursor.close()
+connection.close()
